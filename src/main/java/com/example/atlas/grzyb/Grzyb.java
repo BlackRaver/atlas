7d6154b70437c0b759289d17af6336af140d6351
BlackRaver <blackravern01@gmail.com>
2022-12-27 18:20:14 +0100

    -Added new GrzybController endpoints: "/update/{id}", "/id/{id}", "/add"

--- src/main/java/com/example/atlas/grzyb/Grzyb.java
+++ src/main/java/com/example/atlas/grzyb/Grzyb.java
@@ -3,9 +3,13 @@ package com.example.atlas.grzyb;
 import lombok.*;
 
 import javax.persistence.*;
-
-import static javax.persistence.GenerationType.SEQUENCE;
-
+/**
+ * Klasa Grzyb pełni role modelu danych znanego równiez jako obiekt pojo.
+ * Grzyb przechowuje kluczowe dane potrzebne do funkcjonowania programu.
+ *
+ * Kożystamy z dependency lombok który ułatwia i przyśpiesza pisanie kodu poprzez udostępnianie własnych dekoratorów.
+ *
+ */
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,19 +20,18 @@ import static javax.persistence.GenerationType.SEQUENCE;
 public class Grzyb {
 
     @Id
-    @SequenceGenerator(
-            name = "grzybSequence",
-            sequenceName = "grzybSequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = SEQUENCE,
-            generator = "grzybSequence"
-    )
     private int id;
     private String nazwa;
     private boolean czy_pod_ochrona;
     private boolean czy_trujacy;
     private String wystepowanie;
     private String uwagi;
+
+    public boolean getCzy_pod_ochrona() {
+        return this.czy_pod_ochrona;
+    }
+
+    public boolean getCzy_trujacy() {
+        return this.czy_trujacy;
+    }
 }
